edb62917cafe88c4ce71be9cdba45d834ae5ba31
javabear3388 <jiler3388@gmail.com>
2022-08-22 21:03:12 -0400

    Dependency injection

---
+++ Spring-03-Core-DI/src/main/java/com/spring/OfficeHours.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OfficeHours {
+
+    public int getHours(){
+        return 5;
+    }
+
+}
---
+++ Spring-03-Core-DI/src/main/java/com/spring/TeachingHours.java
@@ -0,0 +1,16 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class TeachingHours {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigApp.class);
+
+        Java java = context.getBean(Java.class);
+        java.getTeachingHours();
+    }
+
+}
