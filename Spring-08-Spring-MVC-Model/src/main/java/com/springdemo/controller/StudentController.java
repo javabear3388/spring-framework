9b0ce19febd45d683847440627c7765795826b6f
javabear3388 <jiler3388@gmail.com>
2022-09-02 07:10:06 -0400

    Thymeleaf practice

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/springdemo/controller/StudentController.java
@@ -0,0 +1,36 @@
+package com.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping("/welcome")
+    public String homePage(Model model){
+        //use model method
+        model.addAttribute("name","model-set-attribute-value");
+        model.addAttribute("course", "MVC");
+
+        String subject = "Collections";
+        model.addAttribute("subject", subject);
+
+        //create some random id (1-1000) and show in UI
+        int studentID = new Random().nextInt(1000);
+        model.addAttribute("id", studentID);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(10);
+        model.addAttribute("numbers",numbers);
+
+        return "/student/welcome"; //no need to put .html cause it's thymleaf template
+    }
+}
