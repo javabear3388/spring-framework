61d7fd83c2a448bbe4ed0c8a1c91fa4e7523e7e7
javabear3388 <jiler3388@gmail.com>
2022-09-15 19:43:56 -0400

    GetMapping tag

--- Spring-09-Spring-Thymleaf/src/main/java/com/springdemo/controller/StudentController.java
+++ Spring-09-Spring-Thymleaf/src/main/java/com/springdemo/controller/StudentController.java
@@ -3,19 +3,22 @@ package com.springdemo.controller;
 import com.springdemo.boostrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register")
+//    @RequestMapping(value = "/register", method= RequestMethod.GET) //GET is default
+    @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
+//    @RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String welcom(Model model){
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/welcome";
