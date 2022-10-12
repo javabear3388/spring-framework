30f753edb58889554236f0a8260f8edb037676f8
javabear3388 <jiler3388@gmail.com>
2022-10-12 18:22:00 -0400

    Let spring boot generate the id

--- Spring-10-ORM-TableCreation/src/main/java/com/springdemo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/springdemo/entity/Student.java
@@ -1,13 +1,18 @@
 package com.springdemo.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName; //at db column name -> first_name
     private String lastName;
     private String email;
