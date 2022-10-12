3a4509b6cfe078ab2fd536398439741e4bac92d2
javabear3388 <jiler3388@gmail.com>
2022-10-12 18:57:35 -0400

    Custom column names

--- Spring-10-ORM-TableCreation/src/main/java/com/springdemo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/springdemo/entity/Student.java
@@ -1,19 +1,18 @@
 package com.springdemo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-
-public class Student {
+@Table(name = "students") //custom table name
+public class Student { //default table name
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "studentFirstName") //-> student_first_name
     private String firstName; //at db column name -> first_name
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
 
