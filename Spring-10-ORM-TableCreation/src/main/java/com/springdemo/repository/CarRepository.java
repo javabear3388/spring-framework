d0d5a8f3ff92038af2a019e44e17c7d25d1c45fe
javabear3388 <jiler3388@gmail.com>
2022-10-17 18:41:29 -0400

    Repository notes

--- Spring-10-ORM-TableCreation/src/main/java/com/springdemo/repository/CarRepository.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/springdemo/repository/CarRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface CarRepository extends JpaRepository<Car, Long> { //class, id
-
+    //lots methods in JpaRepository already
+    //can add more
 
 }
