790932fcf57a0f9f00518332e18e4228299e164c
javabear3388 <jiler3388@gmail.com>
2022-10-24 20:35:52 -0400

    More entities built

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/springdemo/entity/Cinema.java
@@ -0,0 +1,21 @@
+package com.springdemo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cinema extends BaseEntity{
+
+    private String name;
+    private String sponsored_name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Location location;
+
+}
---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/springdemo/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.springdemo.enums;
+
+public enum UserRole {
+    USER, ADMIN;
+}
